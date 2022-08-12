d3a3472e4aec8be40e5877d0743ac76585b2ea17
Rachel Swallow <rswallow987@gmail.com>
2022-08-12 07:41:32 +0200

    feat:MMD gan testing

--- models/MMD_testing.py
+++ models/MMD_testing.py
@@ -4,6 +4,8 @@ from utils import data_sampler2, gen_kde, image_name
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 #out of sample data
 #MMD990009-08-2022-18-15-41.pt - GNet, 2.7% breeches - trained on 5,128 hist samples
@@ -29,35 +31,57 @@ plt.show()
 
 #Backtest
 var95=np.quantile(transformed_noise,0.05)
+var99=np.quantile(transformed_noise,0.01)
 
 k=data_set.reshape(-1).detach().numpy()
 breeches=np.where(k<var95)
 num_breeches=len(breeches[0])
+breeches99=np.where(k<var99)
 
 if num_breeches>len(k)*0.05:
-    print("Out of Sample Breaches %:",num_breeches*100/len(k))
+    print("GAN: Out of Sample Breaches 95%:",num_breeches*100/len(k))
+    print("GAN: Out of Sample Breaches 99%:", len(breeches99[0]) * 100 / len(k))
 else:
-    print("Adequate Model: Out of Sample Breeches %:",num_breeches*100/len(k))
+    print("GAN: Adequate Model: Out of Sample Breeches 95%:",num_breeches*100/len(k))
+    print("GAN: Adequate Model: Out of Sample Breeches 99%:", len(breeches99[0]) * 100 / len(k))
 
 #Backtest in sample
 x=torch.load(f='../data quantiles/MMD_09-08-2022-19-16-06.pt')
 x=x.reshape(-1).detach().numpy()
 breeches_insample=np.where(x<var95)
+breeches_insample99=np.where(x<var99)
 num_breeches_in_sample=len(breeches_insample[0])
 
 if num_breeches_in_sample>len(x)*0.05:
-    print("In Sample Breaches %:",num_breeches_in_sample*100/len(x))
+    print("GAN: In Sample Breaches 95%:",num_breeches_in_sample*100/len(x))
+    print("GAN: Out of Sample Breaches 99%:", len(breeches_insample99[0]) * 100 / len(x))
 else:
-    print("Adequate Model: In Sample Breeches %:",num_breeches_in_sample*100/len(x))
+    print("GAN: Adequate Model: In Sample Breeches 95%:",num_breeches_in_sample*100/len(x))
+    print("GAN: Adequate Model: In Sample Breeches 99%:",  len(breeches_insample99[0]) * 100 / len(x))
 
 #Backtest Historical Model
 mu=x.mean()
 sig=x.std()
 var95_hist=np.quantile(x,0.05)
+var99_hist=np.quantile(x,0.01)
 breeches_hist=np.where(k<var95_hist)
+breeches_hist99=np.where(k<var99_hist)
 num_breeches_hist=len(breeches_hist[0])
 
 if num_breeches_hist>len(x)*0.05:
-    print("In of Sample Breaches (Hist) %:",num_breeches_hist*100/len(x))
+    print("Breaches (Hist) 95%:",num_breeches_hist*100/len(k))
+    print("Breaches (Hist) 99%:", len(breeches_hist99[0]) * 100 / len(k))
 else:
-    print("Adequate Model: In of Sample Breeches (Hist) %:",num_breeches_hist*100/len(x))
+    print("Adequate Model: (Hist) 95%:",num_breeches_hist*100/len(k))
+    print("Adequate Model: (Hist) 99%:",len(breeches_hist99[0]) * 100 / len(k))
+
+#Visualize
+df=pd.DataFrame()
+
+df['Actual']=pd.Series(x.flatten())
+df['Generated']=pd.Series(transformed_noise.flatten())
+fig=sns.kdeplot(df['Actual'], shade=True, color='r')
+fig=sns.kdeplot(df['Generated'], shade=True, color='b')
+plt.legend(labels=["Actual","Generated"])
+plt.xlabel("")
+plt.show()
